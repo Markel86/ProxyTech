a19c66940617cb8e2d524b6bc9be63fcf764bc91
Markel86 <markel5676@gmail.com>
2025-09-22 13:06:39 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+<form method="post" action="home.php"> 
+      <label for="usuario">Usuario:</label><br>
+      <input type="text" name="usuario"> <br>
+      <label for="contrasena">Contraseña:</label><br>
+      <input type="password"  name="contrasena"><br><br>
+      <input type="submit" value="Iniciar Sesión" name="boton">
+</form>
+    </body>
+</html>
