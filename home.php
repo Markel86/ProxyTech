98b2bfa5268f393075039c1a4550cc940b57c8e8
Markel86 <markel5676@gmail.com>
2025-09-22 13:05:08 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,15 @@
+
+<?php
+// echo "Hola, ", $usuario; // Tiene que coincidir con el nombre del input
+
+$usuario = $_POST['usuario'];
+$contrasena = $_POST['contrasena'];
+
+if($usuario == "Paco" && $contrasena == "Porras"){
+        echo "Pacoooo porro";
+} else{
+        echo "solo Paco o contraseña muy mal";
+}
+
+
+?>
