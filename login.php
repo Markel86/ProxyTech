b8cc98c173b671663808eb69ca12961ea1655b0e
jscher006-pixel <jscher006@gmail.com>
2025-09-29 10:54:13 +0200

    asd

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 <form method="post" action="home.php"> 
       <label for="usuario">Usuario sadvdvsdcc:</label><br>
       <input type="text" name="usuario"> <br>
-      <label for="contrasena">Contraseña:</label><br>
+      <label for="contrasena">Contrasenhaa:</label><br>
       <input type="password"  name="contrasena"><br><br>
       <input type="submit" value="Iniciar Sesión" name="boton">
 </form>
